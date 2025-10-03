ba01974e5d6e209b7531e2ef58adc355fd2e7a06
codeKMT <keanumt@gmail.com>
2025-10-03 16:58:28 +0200

    card styling changes

--- webApp/src/components/AccomodationsCard.jsx
+++ webApp/src/components/AccomodationsCard.jsx
@@ -2,10 +2,13 @@ import React from "react";
 
 const AccomodationsCard = ({ title, description, imgSrc }) => {
     return (
-        <div className="accomodationsCard">
-            <img src={imgSrc} alt={title} />
-            <h2>{title}</h2>
+        <div className="border p-4 shadow-lg w-80 mx-auto mt-5 rounded-lg">
+            <img src={imgSrc} alt={title} className="mb-3 rounded-md"/>
+            <div className="accomodationsCardText">
+            <h5 className="text-2xl font-bold border-b-1">{title}</h5>
             <p>{description}</p>
+            <button className="mt-2 bg-black text-white py-1 px-4 rounded hover:bg-gray-800 transition-colors duration-300">More info</button>
+            </div>
         </div>
     );
 };
--- webApp/src/pages/Accomodations.jsx
+++ webApp/src/pages/Accomodations.jsx
@@ -3,27 +3,32 @@ import ImgWrapper from '../components/ImgWrapper';
 import grangeDance from '../assets/img/grangeDance.jpg';
 import AccomodationsCard from '../components/AccomodationsCard';
 import moulinAccomodation from '../assets/img/moulinAccomodation.jpg';
+import vision from '../assets/img/vision.jpg'; 
 
 const Accomodations = () => {
     return (
         <div>
-            
             <ImgWrapper src={moulinAccomodation} alt="AccomodationsPhoto" title="Accomodations" />
+            <div className='accomodationsIntroText p-10 '>
+                <h1 className='text-4xl border-b'>Accomodations at Le Moulin d'Escapat</h1>
+                <p className='text-lg mt-2 '>Discover our charming accommodations nestled in the heart of the French countryside. Whether you're seeking a cozy bed and breakfast, a serene garden retreat, or a rustic caravan experience, Le Moulin d'Escapat offers a variety of options to suit your preferences. Each accommodation is designed to provide comfort and tranquility, allowing you to unwind and immerse yourself in the natural beauty of the surroundings. Explore our offerings below and find the perfect place to stay during your visit.</p>
+
+            </div>
             <div className="accomodationsCardsContainer">
             <AccomodationsCard
                 title="Chambres d'hôtes"
                 description="A beautiful place to stay."
-                imgSrc={grangeDance}
+                imgSrc={vision}
             />
             <AccomodationsCard
                 title="Jardin Du Moulin"
                 description="Comfortable and cozy."
-                imgSrc={grangeDance}
+                imgSrc={vision}
             />
             <AccomodationsCard
                 title="Caravans du Moulin"
                 description="Experience the countryside."
-                imgSrc={grangeDance}
+                imgSrc={vision}
             />
             </div>
 
